2cfff14002059cb434d8efb06b571d6ad6e197f0
Carlos che <car123che@gmail.com>
2021-05-13 12:12:56 -0600

    spreedshet, dividir imagenes

--- src/template.js
+++ src/template.js
@@ -1,6 +1,6 @@
 GamePlayManager = {
     init: function(){ //se llama de primero, podemos inicializar variables
-        game.scale.scaleMode = Phase.ScaleManager.SHOW_ALL; //para escalar la pantalla
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; //para escalar la pantalla
         //para alinear horizontal y verticalmente
         game.scale.pageAlignHorizontally = true;
         game.scale.pageAlignVertically = true;
@@ -9,10 +9,21 @@ GamePlayManager = {
         game.load.image('background', './assets/images/background.png'); //asi se carga una imagen
         //el primer parametro es el nombre de instancia para identeificar la imagen
         //el segundo es la ruta
+        game.load.spritesheet('horse', './assets/images/horse.png', 84, 156, 2);
+        //spritesheet es para dividir una imagen en dos, se ponee el identificado
+        //luego la ruta, luego ancho y alto de la imagen a dividir:
+        //la imagen media 168 entonce su mitad de ancho es 84  y por utlimo
+        //va en cuanto lo queriamos dividir en este caso en 2
     },
     create: function(){// tenemos todo cargados los recursos y poderlos usar
         game.add.sprite(0, 0, 'background'); //asi se agrega una imagen, primeo debe estar cargada
         //primero se ponen las coordenadas donde se quiere cargar y luego que imagen se desea cargar
+        this.horse = game.add.sprite(0,0, 'horse'); //guardamos una instancia del spritesheet creado
+        this.horse.frame = 1; //.frame = es la imagen selecionada
+        /*centramos la imagen al centro */
+        this.horse.x = game.width/2;
+        this.horse.y= game.height/2;
+        
     },
     update: function(){//frame a frame se llama este metodo
         
